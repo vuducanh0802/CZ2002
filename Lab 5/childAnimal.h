dfc73b752296df8e67a233e29e14346f067419e4
Vu Duc Anh <67143004+vuducanh0802@users.noreply.github.com>
2021-11-04 14:57:50 +0800

    Create childAnimal.h

---
+++ Lab 5/childAnimal.h
@@ -0,0 +1,29 @@
+#ifndef CHILD_ANIMAL_H
+#define CHILD_ANIMAL_H
+#include "animal.h"
+
+class Dog : public Mammal {
+public:
+    Dog(string n, COLOR c, string owner);
+    ~Dog();
+    void speak() const;
+    void move();
+};
+
+class Cat : public Mammal {
+public:
+    Cat(string n, COLOR c, string owner);
+    ~Cat();
+    void speak() const;
+    void move();
+};
+
+class Lion : public Mammal {
+public:
+    Lion(string n, COLOR c, string owner);
+    ~Lion();
+
+    void speak() const;
+    void move();
+};
+#endif
