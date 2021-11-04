ecb5488064eed5c0eeb7ef830b2acb8a17d5c568
Vu Duc Anh <67143004+vuducanh0802@users.noreply.github.com>
2021-11-04 14:57:33 +0800

    Create childAnimal.cpp

---
+++ Lab 5/childAnimal.cpp
@@ -0,0 +1,48 @@
+#include "childAnimal.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Dog::Dog(string n, COLOR c, string owner) : Mammal(n, c) {
+    owner = owner;
+}
+
+Dog::~Dog() = default;
+
+void Dog::speak() const {
+    cout << "Woof" << endl;
+}
+
+void Dog::move() {
+    cout << "Dog moves" << endl;
+}
+
+
+Cat::Cat(string n, COLOR c, string owner) : Mammal(n, c) {
+    owner = owner;
+}
+
+Cat::~Cat() = default;
+
+void Cat::speak() const {
+    cout << "Meow" << endl;
+}
+
+void Cat::move() {
+    cout << "Cat moves" << endl;
+}
+
+Lion::Lion(string n, COLOR c, string owner) : Mammal(n, c) {
+    owner = owner;
+}
+
+Lion::~Lion() = default;
+
+void Lion::speak() const {
+    cout << "Roar" << endl;
+}
+
+void Lion::move() {
+    cout << "Lion moves" << endl;
+}
